27451d3a43b7bf08956e4b96e8afec508c2eceb5
Your Name <your_email@real_email.edu>
2020-08-28 23:18:41 -0400

    UserHub

---
+++ build/precache-manifest.6e15c14393becd01d7ac8de02897ce49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b14e6bfb904b3fb332a32b6611953a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69d560b1b2ad2a4bb40e",
+    "url": "/static/css/main.6b761ef3.chunk.css"
+  },
+  {
+    "revision": "f98c157011c80e910a79",
+    "url": "/static/js/2.4776ca5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4776ca5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d560b1b2ad2a4bb40e",
+    "url": "/static/js/main.facd53da.chunk.js"
+  },
+  {
+    "revision": "e44ca8050686401cca12",
+    "url": "/static/js/runtime-main.13213274.js"
+  }
+]);
